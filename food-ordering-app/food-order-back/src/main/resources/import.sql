b445800d359b3ab61494888cae6ee7349c4429a5
KirtiGupta-22 <kg474134@gmail.com>
2025-04-23 14:03:59 +0530

    Initial commit with food ordering app

--- food-ordering-app/food-order-back/src/main/resources/import.sql
+++ food-ordering-app/food-order-back/src/main/resources/import.sql
@@ -26,6 +26,40 @@ INSERT INTO order_item (id, meal_description, meal_image, meal_image_name, meal_
 (2, 'Spicy grilled chicken burger', 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg', 'burger.jpg', 'Chicken Burger', 150, 'Burger', 1, 1, 2),
 (3, 'Toasted sandwich with paneer', 'https://cdn.pixabay.com/photo/2019/04/08/18/35/sandwich-4112566_640.jpg', 'sandwich.jpg', 'Paneer Sandwich', 120, 'Sandwich', 1, 1, 3);
 
+-- Inserting meal types
+INSERT INTO meal_type (id, type_name, image, image_name, description, deleted) VALUES
+(1, 'Pizza', 'https://cdn.pixabay.com/photo/2018/01/15/07/51/pizza-3082000_1280.jpg', 'pizza.jpg', 'Delicious pizzas', false),
+(2, 'Burger', 'https://cdn.pixabay.com/photo/2016/11/19/12/00/hamburger-1840450_1280.jpg', 'burger.jpg', 'Tasty burgers', false),
+(3, 'Sandwich', 'https://cdn.pixabay.com/photo/2016/11/29/09/08/sandwich-1869740_1280.jpg', 'sandwich.jpg', 'Fresh Sandwiches', false),
+(4, 'Biryani', 'https://cdn.pixabay.com/photo/2016/11/29/09/08/biryani-1869741_1280.jpg', 'biryani.jpg', 'Flavorful Biryani', false),
+(5, 'Thali', 'https://cdn.pixabay.com/photo/2016/11/29/09/08/thali-1869742_1280.jpg', 'thali.jpg', 'Complete Meals', false);
+
+-- Inserting meals
+INSERT INTO meal (id, name, price, image, image_name, description, meal_type_id) VALUES
+(6, 'Pepperoni Pizza', 220, 'https://cdn.pixabay.com/photo/2018/01/15/07/51/pizza-3082000_1280.jpg', 'pizza.jpg', 'Classic pepperoni pizza with extra cheese', 1),
+(7, 'Veggie Burger', 130, 'https://cdn.pixabay.com/photo/2016/11/19/12/00/hamburger-1840450_1280.jpg', 'burger.jpg', 'Grilled veggie burger with fresh toppings', 2),
+(8, 'Club Sandwich', 150, 'https://cdn.pixabay.com/photo/2016/11/29/09/08/sandwich-1869740_1280.jpg', 'sandwich.jpg', 'Triple-decker sandwich with turkey and bacon', 3),
+(9, 'Vegetable Biryani', 230, 'https://cdn.pixabay.com/photo/2016/11/29/09/08/biryani-1869741_1280.jpg', 'biryani.jpg', 'Aromatic rice with mixed vegetables', 4),
+(10, 'Punjabi Thali', 350, 'https://cdn.pixabay.com/photo/2016/11/29/09/08/thali-1869742_1280.jpg', 'thali.jpg', 'A full platter of Punjabi delicacies', 5);
+
+-- New meal types
+INSERT INTO meal_type (id, type_name, image, image_name, description, deleted) VALUES
+(6, 'Pasta', 'https://cdn.pixabay.com/photo/2018/01/15/07/51/pasta-3082001_1280.jpg', 'pasta.jpg', 'Delicious Pasta Dishes', false),
+(7, 'Salad', 'https://cdn.pixabay.com/photo/2016/11/19/12/00/salad-1840451_1280.jpg', 'salad.jpg', 'Fresh and Healthy Salads', false),
+(8, 'Dessert', 'https://cdn.pixabay.com/photo/2016/11/19/12/00/dessert-1840452_1280.jpg', 'dessert.jpg', 'Sweet Treats', false),
+(9, 'Soup', 'https://cdn.pixabay.com/photo/2016/11/19/12/00/soup-1840453_1280.jpg', 'soup.jpg', 'Warm and Comforting Soups', false),
+(10, 'Seafood', 'https://cdn.pixabay.com/photo/2016/11/19/12/00/seafood-1840454_1280.jpg', 'seafood.jpg', 'Fresh Seafood Dishes', false);
+
+-- New meals
+INSERT INTO meal (id, name, price, image, image_name, description, meal_type_id) VALUES
+(11, 'Spaghetti Carbonara', 250, 'https://cdn.pixabay.com/photo/2018/01/15/07/51/pasta-3082001_1280.jpg', 'pasta.jpg', 'Classic Italian pasta with creamy sauce and pancetta', 6),
+(12, 'Caesar Salad', 180, 'https://cdn.pixabay.com/photo/2016/11/19/12/00/salad-1840451_1280.jpg', 'salad.jpg', 'Crisp romaine lettuce with Caesar dressing and croutons', 7),
+(13, 'Chocolate Cake', 150, 'https://cdn.pixabay.com/photo/2016/11/19/12/00/dessert-1840452_1280.jpg', 'dessert.jpg', 'Rich and moist chocolate cake with frosting', 8),
+(14, 'Tomato Basil Soup', 120, 'https://cdn.pixabay.com/photo/2016/11/19/12/00/soup-1840453_1280.jpg', 'soup.jpg', 'Creamy tomato soup with fresh basil', 9),
+(15, 'Grilled Salmon', 350, 'https://cdn.pixabay.com/photo/2016/11/19/12/00/seafood-1840454_1280.jpg', 'seafood.jpg', 'Perfectly grilled salmon served with lemon', 10);
+
+
+
 
 
 
