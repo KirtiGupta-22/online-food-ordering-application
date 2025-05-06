56e29b4429ac098c8cb7386309ca8d273cc7ff0c
KirtiGupta-22 <kg474134@gmail.com>
2025-05-07 00:42:01 +0530

    update file changes

--- food-ordering-app/food-ordering-front/src/services/LoginService.js
+++ food-ordering-app/food-ordering-front/src/services/LoginService.js
@@ -3,12 +3,12 @@ import TokenService from "./TokenService";
 
 class LoginService{
     login(loginParams){
-        return axios.post("https://localhost:8080/api/login", loginParams);
+        return axios.post("http://localhost:8080/api/login", loginParams);
     }
 
     logout(){
         TokenService.setTokenInHeader();
-        return axios.get("https://localhost:8080/api/logout");
+        return axios.get("http://localhost:8080/api/logout");
     }
 }
 
--- food-ordering-app/food-ordering-front/src/services/MealTypeService.js
+++ food-ordering-app/food-ordering-front/src/services/MealTypeService.js
@@ -4,21 +4,21 @@ import TokenService from "./TokenService";
 class MealTypeService{
 
     getAllMealTypes(){
-        return axios.get("https://localhost:8080/api/mealType/getAllMealTypes");
+        return axios.get("http://localhost:8080/api/mealType/getAllMealTypes");
     }
     createMealType(fd){
         TokenService.setTokenInHeader();
-        return axios.post("https://localhost:8080/api/mealType/createMealType", fd);
+        return axios.post("http://localhost:8080/api/mealType/createMealType", fd);
     }
 
     deleteMealType(mealTypeId){
         TokenService.setTokenInHeader();
-        return axios.put("https://localhost:8080/api/mealType/deleteMealType/" + mealTypeId);
+        return axios.put("http://localhost:8080/api/mealType/deleteMealType/" + mealTypeId);
     }
 
     updateMealType(mealType){
         TokenService.setTokenInHeader();
-        return axios.put("https://localhost:8080/api/mealType/updateMealType", mealType);
+        return axios.put("http://localhost:8080/api/mealType/updateMealType", mealType);
     }
 
 }
